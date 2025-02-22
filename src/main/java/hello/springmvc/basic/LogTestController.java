2059be36fa9606262bbe6602c10e4b980dd3763b
limchaehwan <chlim20@naver.com>
2025-02-22 14:39:08 +0900

    로깅 알아보기

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,30 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LogTestController {
+
+    //private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+
+        //로그를 사용하지 않아도 리소스를 사용하기 때문에 메모리 낭비
+        log.debug("String concat log=" + name);
+
+        return "ok";
+
+    }
+}
